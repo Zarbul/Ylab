ca04a4215ef4b43049d706c3cc26cbe2aa98daa7
Michael Kamenev <dlyapicem@gmail.com>
2022-06-28 20:04:12 +0300

    изменил вывод на стринг

--- homework-1/2.py
+++ homework-1/2.py
@@ -1,9 +1,9 @@
 from ipaddress import IPv4Address
 
 
-def int32_to_ip(int32: int) -> IPv4Address:
+def int32_to_ip(int32: int) -> str:
     ip = IPv4Address(int32)
-    return ip
+    return str(ip)
 
 
 if __name__ == '__main__':
